e117c6d51d75d4ef6037795616d0ff9ca477d34c
Lino09 <onosmx@gmail.com>
2024-01-19 16:42:08 -0600

    fix: timeout to allow cookie being set

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -24,7 +24,9 @@ export default function Login() {
       const data = await authService.login();
       const { guest_session_id } = data;
       document.cookie = `userSession=${guest_session_id}; max-age=3600`;
-      router.push("/");
+      setTimeout(() => {
+        router.push("/")
+      }, 1000)
     } catch (err) {
       console.log(err);
     }
